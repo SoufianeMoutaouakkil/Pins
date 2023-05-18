7da2f941cb6a0f6dbf708aeb1e7b5f42aea74400
soufiane moutaouakkil <soufiane.moutaouakkil.ma@gamil.com>
2023-05-18 20:51:21 +0100

    improve - protect PinController

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -6,11 +6,13 @@ use App\Entity\Pin;
 use App\Form\PinType;
 use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[IsGranted("ROLE_USER")]
 class PinController extends AbstractController
 {
     #[Route('/pin', name: 'pin_index')]
